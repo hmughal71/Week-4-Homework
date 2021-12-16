b216b836a85e25c7f616a8be2486fe612fcec4ed
hmughal71 <johnsamdough@gmail.com>
2021-12-16 19:14:44 +1100

    fixed some errors on JS during office hour with Chris and Takuji's help

--- script.js
+++ script.js
@@ -8,48 +8,49 @@ var currentQuestion = {}
 var acceptingAnswers = true
 var score = 0
 var questionCounter = 0
+
 var questions = [
     {
-        question: "Commonly used DATA types DO NOT include",
-        choice1: "strings",
-        choice2: "boolean",
-        choice3: "alert",
-        choice4: "number",
-        answer: "alert",
-    },
+        question: Commonly used DATA types DO NOT include:
+        choice1 "strings",
+        choice2 "boolean",
+        choice3 "alert",
+        choice4 "number",
+        answer: alert,
+    }
     {
-        question: "The condition in an if/else statement is enclosed withing _______",
-        choice1: "quotes",
-        choice2: "curly brackets",
-        choice3: "parentheses",
-        choice4: "square brackets",
-        answer: "curly brackets",
-    },
+        question: The condition in an if/else statement is enclosed withing _______:
+        choice1 "quotes",
+        choice2 "curly brackets",
+        choice3 "parentheses",
+        choice4 "square brackets",
+        answer: curly brackets,
+    }
     {
-        question: "Arrays in Javascripts can be used to store ______",
-        choice1: "numbers and strings",
-        choice2: "other arrays",
-        choice3: "boolean",
-        choice4: "all of above",
-        answer: "all of above",
+        question: Arrays in Javascripts can be used to store _______:
+        choice1 "numbers and strings",
+        choice2 "other arrays",
+        choice3 "boolean",
+        choice4 "all of above",
+        answer: all of above,
 
-    },
+    }
     {
-        question: "String values must be enclosed within _______ when being assigned to variables",
-        choice1: "commas",
-        choice2: "curly brackets",
-        choice3: "quotes",
-        choice4: "parentheses",
-        answer: "quotes",
-    },
+        question: String values must be enclosed within _______ when being assigned to variables:
+        choice1 "commas",
+        choice2 "curly brackets",
+        choice3 "quotes",
+        choice4 "parentheses",
+        answer: quotes,
+    }
     {
-        question: "A very useful tool used during development and debugging for printing content to the debugger is",
-        choice1: "Javascript",
-        choice2: "terminal / bash",
-        choice3: "for loops",
-        choice4: "console.log",
-        answer: "console.log",
-    },
+        question: A very useful tool used during development and debugging for printing content to the debugger is:
+        choice1 "Javascript",
+        choice2 "terminal / bash",
+        choice3 "for loops",
+        choice4 "console.log",
+        answer: console.log,
+    }
 ]
 
 var SCORE_POINTS = 100
@@ -109,9 +110,3 @@ choices.forEach(choices => {
         }, 1000)
     })
 })
-
-incrementScore = num => {
-    score +=numscoreText.innerText = score
-}
-
-startQuiz()
